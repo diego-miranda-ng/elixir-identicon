dfb0e3a3d7f89cf4c7f8ab9ac38a64735899cb0b
Diego Miranda Neves Gomes <diego.miranda.ng@gmail.com>
2021-05-20 21:29:33 -0300

    Filter the odd values

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,7 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
@@ -17,8 +18,17 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter(grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end)
+
+    %Identicon.Image{image | grid: grid}
+  end
+
   def mirror_row(row) do
     [first, second | _tail] = row
+
     row ++ [first, second]
   end
 
